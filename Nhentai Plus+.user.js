fab9261d94fc540d844b68713c85bf3fc218e605
longkidkoolstar <75497675+longkidkoolstar@users.noreply.github.com>
2024-06-22 11:25:48 -0400

    Find alt version button appending tofavorites fixed

--- Nhentai Plus+.user.js
+++ Nhentai Plus+.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Nhentai Plus+
 // @namespace    github.com/longkidkoolstar
-// @version      2.1
+// @version      3.0
 // @description  Enhances the functionality of Nhentai website.
 // @author       longkidkoolstar
 // @match        https://nhentai.net/*
