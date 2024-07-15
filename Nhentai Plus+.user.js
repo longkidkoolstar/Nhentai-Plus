e489d9eed3bacad20423985d2ed4edb1ac76aa92
longkidkoolstar <75497675+longkidkoolstar@users.noreply.github.com>
2024-07-15 18:32:37 -0400

    changed version number because I forgot to do that

--- Nhentai Plus+.user.js
+++ Nhentai Plus+.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Nhentai Plus+
 // @namespace    github.com/longkidkoolstar
-// @version      3.3
+// @version      3.4
 // @description  Enhances the functionality of Nhentai website.
 // @author       longkidkoolstar
 // @match        https://nhentai.net/*
