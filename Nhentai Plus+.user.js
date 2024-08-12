f18bb5086bcfa137c99165ceb984c316f04af195
longkidkoolstar <75497675+longkidkoolstar@users.noreply.github.com>
2024-08-11 22:53:13 -0400

    -random hentai preferences -updated version number -bug fixes -additional features(popups and whatnot)

--- Nhentai Plus+.user.js
+++ Nhentai Plus+.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Nhentai Plus+
 // @namespace    github.com/longkidkoolstar
-// @version      3.5
+// @version      4.0
 // @description  Enhances the functionality of Nhentai website.
 // @author       longkidkoolstar
 // @match        https://nhentai.net/*
